8bbed3ec248747a213221a6350e6076f85399972
Ryan Johnson <rjohnson@rockefeller.edu>
2026-02-12 16:38:42 -0500

    making a program the reads a file and is able to only work with data that is organized in the way I want. I want the first entry in the line to be a string, second, third, and fourth entry to be a double. Any line that does not have that should be ignored. Then I kept track of any lines that could not be processed becasue of unexpected data

--- employee_wages/main.cpp
+++ employee_wages/main.cpp
@@ -14,6 +14,45 @@ int main()
     ifstream infile;
     infile.open(filename);
 	if (infile) {
+        string name;
+        double hourly_wage;
+        double hours;
+        double total;
+        string line;
+        int loop_num = 0;
+        int processed_num = 0 ;
+        
+        // need to put this outside the while loop so it is only displayed once
+        cout << fixed << setprecision(2)
+        << left << setw(10) << "Name" << right << setw(10) <<  "Hourly wage" << setw(10) << "Hours" << setw(10) << "Total" << endl << endl;
+        // this code below is attempting to get the bars that separate the header above and the data below
+        cout << setw(10) << setfill('-') ;
+        cout << setw(11) << "- " << setw(11) << "- " << setw(11) << "- " << setw(11) << "- "<< endl;
+        cout << setfill(' ');
+        
+        while (getline(infile, line)){
+            stringstream ss;
+            ss.str(line);
+            ss.clear();
+            
+            if (infile.eof()){
+                cout << "You have reached the end of the file. Bye!" << endl;
+                break;
+            }
+            else if (ss >> name >> hourly_wage >> hours){
+                
+                total = hourly_wage * hours;
+//                cout << fixed << setprecision(2)
+//                << setw(10) << "Name" << setw(8) <<  "Hourly wage" << setw(8) << "Hours" << setw(8) << "Total" << endl << endl;
+                cout << left << setw(10) << name << right << setw(10) << hourly_wage << setw(10) << hours << setw(10) << total << endl;
+                processed_num += 1;
+            }
+            loop_num += 1;
+            
+        }
 		infile.close();
+        
+        cout << loop_num - processed_num << " lines not processed\n";
+        
 	}
 }
